84f17fc235db014e09e3480641ef5259c576f981
tuzhu <38799977+tuzhuzi@users.noreply.github.com>
2024-08-14 15:36:34 +0800

    daily commit lc 3152

---
+++ daily/lc3152.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+
+/**
+ * https://leetcode.cn/problems/special-array-ii/
+ * 思路是将特性表记录下来，通过求和取代异或运算
+ */
+vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {}
